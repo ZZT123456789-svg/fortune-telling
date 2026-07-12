0cb29b1fd115c53245fef5497b5a370c90d3763e
Administrator <administrator@fortune-telling.com>
2026-07-12 18:57:55 +0800

    切回api.z-pay.cn

--- api/alipay.js
+++ api/alipay.js
@@ -53,7 +53,7 @@ module.exports = async function handler(req, res) {
     const postData = querystring.stringify(zpayParams);
     const result = await new Promise(function(resolve, reject) {
       var req2 = https.request({
-        hostname: 'pay.z-pay.cn',
+        hostname: 'api.z-pay.cn',
         path: '/submit.php',
         method: 'POST',
         headers: {
